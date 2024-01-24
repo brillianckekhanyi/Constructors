b6ac9bd787197ac124ad8924e21e1fb65ad054d0
Dell <Dell@DESKTOP-HKF2PQO>
2024-01-24 08:05:04 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book ("Harry potter", "JK Rowling", 400);
+            Console.WriteLine(book1.title);
+            Console.ReadLine();
+        }
+
+    }
+    class Book
+    {
+        public string title;
+        public string author;
+        public int pages;
+
+        public Book(string aTitle,string aAuthor,int aPages)
+        {
+            title = aTitle;
+            author = aAuthor;
+            pages = aPages;
+
+        }
+
+
+    }
+}
